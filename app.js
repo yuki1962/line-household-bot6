09dc8250230ec2f2fb9f92528a1cc889d8dea36c
yuki1962 <orange.tail12@gmail.com>
2025-07-17 15:57:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const config = {
-  channelAccessToken: "MF0W2sLvZHnGzYqC1TrKLCZUCtT/LJYO7jxuJyxa0PPXIKM8YW+dadnCzMoxNXowssHnRWEdFglFrKQ5vyvRqggxQtLbkrQUot/vLY3Uf5VKGGK/Oh/plIg5sHLs6aA/vKshB8q/kr0e/AcIHl/9iwdB04t89/1O/w1cDnyilFU=",
+  channelAccessToken: "MF0W2sLvZHnGzYqC1TrKLCZUCtT/LJYO7jxuJyxa0PPXIKM8YW+dadnCzMoxNXowssHnRWEdFglFrKQ5vyvRqggxQtLbkrQUot/vLY3Uf5VKGGK/Oh/plIg5sHLs6aA/vKshB8q/kr0e/AcIHl/9iwdB04t89/1O/w1cDnyilFU=";
   channelSecret: "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";
 
 const client = new Client(config);
